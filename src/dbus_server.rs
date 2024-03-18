5bde6b37fece7f03027c2e4bd3609e19b2f943a7
Jeremy Reed <reeje76@gmail.com>
2024-03-18 16:30:47 -0400

    Use a message channel to pass messages.
    Pass messages back to the main thread when we get dbus events.

--- src/dbus_server.rs
+++ src/dbus_server.rs
@@ -1,21 +1,26 @@
+use std::sync::mpsc::Sender;
 use zbus::{connection::Builder, interface, Connection, Result};
 use event_listener::{Event, Listener};
 
 struct DbusServer {
+    tx: Sender<String>,
     done: Event,
 }
 
 #[interface(name = "com.thetechforest.WallsD1")]
 impl DbusServer {
     async fn say_hello(&self, name: &str) -> String {
+        self.tx.send(String::from("Foo Bar")).unwrap();
+
         println!("say_hello called with name: {}", name);
         format!("Hello, {}!", name)
     }
 }
 
-pub async fn run_server() -> Result<()> {
+pub async fn run_server(tx: Sender<String>) -> Result<()> {
 
     let dbus_server = DbusServer {
+        tx,
         done: event_listener::Event::new(),
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 use futures::executor::block_on;
@@ -8,13 +9,19 @@ mod dbus_server;
 async fn main() {
     println!("wallsd started");
 
+    let (tx, rx) = mpsc::channel::<String>();
+
     thread::spawn(|| {
         println!("Starting dbus server");
-        let _ = block_on(dbus_server::run_server());
+        let _ = block_on(dbus_server::run_server(tx));
     });
 
     loop {
         println!("Listening for dbus events!");
+        match rx.try_recv() {
+            Ok(message) => println!("Got message: {}", message),
+            Err(_) => println!("No message"),
+        };
         thread::sleep(Duration::from_secs(10));
     }
 }
