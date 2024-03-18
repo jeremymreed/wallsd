a53f586f590ab1d3e4ecfa2d621f673aeb9a01a3
Jeremy Reed <reeje76@gmail.com>
2024-03-18 16:07:34 -0400

    Run dbus server on separate thread.
    Use block_on() executor to run the Future on a separate thread.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use std::thread;
+use std::time::Duration;
+use futures::executor::block_on;
+
 mod dbus_server;
 
 #[async_std::main]
 async fn main() {
     println!("wallsd started");
 
-    dbus_server::run_server().await;
+    thread::spawn(|| {
+        println!("Starting dbus server");
+        let _ = block_on(dbus_server::run_server());
+    });
+
+    loop {
+        println!("Listening for dbus events!");
+        thread::sleep(Duration::from_secs(10));
+    }
 }
