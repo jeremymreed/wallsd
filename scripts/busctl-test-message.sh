515c6bacf3af209cd4d4d910f0415ded799f5882
Jeremy Reed <reeje76@gmail.com>
2024-03-20 10:14:55 -0400

    Add a wrapper script to call Test via busctl.

---
+++ scripts/busctl-test-message.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+#
+
+busctl --user call \
+    com.thetechforest.WallsD \
+    /com/thetechforest/WallsD \
+    com.thetechforest.WallsD \
+    Test \
+    ii \
+    32 \
+    64 
