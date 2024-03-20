b44542a8870680d8edf421b024e25b5f819f41a7
Jeremy Reed <reeje76@gmail.com>
2024-03-20 10:12:28 -0400

    Fix test() method.
    We were using {Serialize, Deserialize}Dict from zvariant, when we should
    have been using Serialize, Deserialize from serde.  We still need
    zvariant's Type.  This caused problems when using clients to communicate
    with wallsd.

--- src/dbus_server.rs
+++ src/dbus_server.rs
@@ -1,19 +1,31 @@
 use std::sync::mpsc::Sender;
-use zbus::{connection::Builder, interface, Connection, Result};
+use zbus::{connection::Builder, interface, Result};
+use zvariant::Type;
+use serde::{Deserialize, Serialize};
 use event_listener::{Event, Listener};
+
+#[derive(Serialize, Deserialize, Type, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
 struct DbusServer {
     tx: Sender<String>,
     done: Event,
 }
 
-#[interface(name = "com.thetechforest.WallsD1")]
+#[interface(name = "com.thetechforest.WallsD")]
 impl DbusServer {
     async fn say_hello(&self, name: &str) -> String {
         self.tx.send(String::from("Foo Bar")).unwrap();
 
         println!("say_hello called with name: {}", name);
         format!("Hello, {}!", name)
+    }
+
+    async fn test(&self, point: Point) {
+        println!("test called with point: {:#?}", point);
     }
 }
 
