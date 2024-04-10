5b36143a871d3e6d6728b41d88561cc21eac0785
Jeremy Reed <reeje76@gmail.com>
2024-04-10 10:55:12 -0400

    Add check to see if an output has no images.
    The output's image vector may be empty.  Skip setting the wallpaper in
    this case.

--- src/swww.rs
+++ src/swww.rs
@@ -3,21 +3,26 @@ use rand::Rng;
 use crate::output::Output;
 
 pub fn set_wallpaper(output: &Output) {
-    let random_index = rand::thread_rng().gen_range(0..output.images.len());
 
-    let random_wallpaper = output.images.get(random_index).unwrap();
+    if !output.images.is_empty() {
+        let random_index = rand::thread_rng().gen_range(0..output.images.len());
 
-    let swww_output = Command::new("swww")
-        .arg("img")
-        .arg("-o")
-        .arg(&output.name)
-        .arg(random_wallpaper)
-        .output()
-        .expect("failed to execute process");
+        let random_wallpaper = output.images.get(random_index).unwrap();
 
-    tracing::debug!("output: {}", output.name);
-    tracing::debug!("random_wallpaper: {}", random_wallpaper);
-    tracing::debug!("status: {}", swww_output.status);
-    tracing::debug!("stdout: {}", String::from_utf8_lossy(&swww_output.stdout));
-    tracing::debug!("stderr: {}", String::from_utf8_lossy(&swww_output.stderr));
+        let swww_output = Command::new("swww")
+            .arg("img")
+            .arg("-o")
+            .arg(&output.name)
+            .arg(random_wallpaper)
+            .output()
+            .expect("failed to execute process");
+
+        tracing::debug!("output: {}", output.name);
+        tracing::debug!("random_wallpaper: {}", random_wallpaper);
+        tracing::debug!("status: {}", swww_output.status);
+        tracing::debug!("stdout: {}", String::from_utf8_lossy(&swww_output.stdout));
+        tracing::debug!("stderr: {}", String::from_utf8_lossy(&swww_output.stderr));
+    } else {
+        tracing::warn!("No images found for output: {}", output.name);
+    }
 }
