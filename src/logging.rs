326e3adc3c78ae8bb4f3d525053348096f2a948c
Jeremy Reed <reeje76@gmail.com>
2024-04-17 19:22:57 -0400

    Set logging threshold to TRACE.

--- src/logging.rs
+++ src/logging.rs
@@ -5,7 +5,7 @@ pub fn init() {
     let log_path = shellexpand::tilde("~/.local/state/walls/log").into_owned();
     let file_appender = tracing_appender::rolling::daily(log_path, "wallsd.log");
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::DEBUG)
+        .with_max_level(Level::TRACE)
         .with_writer(file_appender)
         .finish();
 
