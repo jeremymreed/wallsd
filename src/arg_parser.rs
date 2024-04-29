f6f94f89da68382971d7c235dc72d053c284b8cb
Jeremy Reed <reeje76@gmail.com>
2024-04-29 13:35:17 -0400

    Complete version option.
    Pull the version from the Cargo.toml, pre-pend a 'v', and add the commit hash.
    Version string format: v<version from Cargo.toml>-<git commit hash>

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -1,8 +1,14 @@
 use clap::Command;
+use lazy_static::lazy_static;
+use crate::build;
 
 pub fn parse_args() {
+    lazy_static! {
+        static ref PKG_VERSION: String = format!("v{}-{}", build::PKG_VERSION, build::COMMIT_HASH);
+    }
+
     Command::new("wallsd")
-        .version("v0.0.0")
+        .version(PKG_VERSION.as_str())
         .about(clap::crate_description!())
         .get_matches();
 }
