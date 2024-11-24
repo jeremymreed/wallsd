ebcaf573a92bf1c403b523ee8bf2a810bae22c88
Jeremy Reed <reeje76@gmail.com>
2024-11-24 15:40:48 -0500

    Update swww::set_wallpaper.
    Call output.get_next_wallpaper.
    Remove code for setting wallpaper, since it has been moved to Output.

--- src/swww.rs
+++ src/swww.rs
@@ -1,30 +1,19 @@
 use std::process::Command;
-use rand::Rng;
-use crate::mode::Mode;
 use crate::output::Output;
 
 /* Chooses a random wallpaper, and then changes the wallpaper. */
-pub fn set_wallpaper(output: &Output) -> String {
+pub fn set_wallpaper(output: &mut Output) {
 
     if !output.images.is_empty() {
         // TODO: The index should be stored in each output.
         // Shouldn't generate or set the index here.
-        let index = match output.mode {
-            Mode::Slideshow => {
-                rand::thread_rng().gen_range(0..output.images.len())
-            },
-            Mode::Oneshot => {
-                0
-            }
-        };
-
-        let wallpaper = output.images.get(index).unwrap();
+        let wallpaper = output.get_next_wallpaper();
 
         let swww_output = Command::new("swww")
             .arg("img")
             .arg("-o")
             .arg(&output.name)
-            .arg(wallpaper)
+            .arg(&wallpaper)
             .output()
             .expect("failed to execute process");
 
@@ -33,10 +22,9 @@ pub fn set_wallpaper(output: &Output) -> String {
         tracing::debug!("status:    {}", swww_output.status);
         tracing::debug!("stdout:    {}", String::from_utf8_lossy(&swww_output.stdout));
         tracing::debug!("stderr:    {}", String::from_utf8_lossy(&swww_output.stderr));
-
-        wallpaper.to_owned()
     } else {
+        // In the case there are no images loaded.
         tracing::warn!("No images found for output: {}", output.name);
-        String::from("")
+        // Do nothing for now.
     }
 }
