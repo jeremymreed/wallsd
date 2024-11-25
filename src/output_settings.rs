9181a6dffb3f6fb87650d9e277f221c94c46b0c3
Jeremy Reed <reeje76@gmail.com>
2024-11-25 17:47:07 -0500

    Add current_index to OutputSettings.

--- src/output_settings.rs
+++ src/output_settings.rs
@@ -10,6 +10,7 @@ pub struct OutputSettings {
     pub mode: mode::Mode,
     pub oncalendar: String,
     pub current_wallpaper: String,
+    pub current_index: u64,
     pub number_of_images: u64,
     pub images: Vec<String>,
 }
